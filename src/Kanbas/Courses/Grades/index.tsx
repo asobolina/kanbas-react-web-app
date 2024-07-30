866100bea32f4519e3ffb305de84e7eafba583d2
asobolina <sobolina.a@northeastern.edu>
2024-07-29 20:14:57 -0500

    path issues

--- src/Kanbas/Courses/Grades/index.tsx
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import GradesControls from './GradesControls';
+import React from "react";
+import GradesControls from "./GradesControls";
 import { CiFilter } from "react-icons/ci";
 import { IoIosArrowDown } from "react-icons/io";
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
-
-
-
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 export default function Grades() {
+  const params = useParams();
+  const courseId = params.id;
+
+  // Get the list of students enrolled in the current course
+  const enrolledStudents = db.enrollments
+    .filter((enrollment) => enrollment.course === courseId)
+    .map((enrollment) => db.users.find((user) => user._id === enrollment.user));
+
+  // Get the list of assignments for the current course
+  const courseAssignments = db.assignments.filter(
+    (assignment) => assignment.course === courseId
+  );
+
   return (
     <div className="container mt-4">
       <GradesControls />
@@ -15,55 +27,77 @@ export default function Grades() {
       <div className="row mb-3">
         <div className="col-lg-6">
           <div className="input-group">
-            <span className="input-group-text"><HiMiniMagnifyingGlass /></span>
-            <input type="text" className="form-control" placeholder="Search Students" />
-            <span className="input-group-text"><IoIosArrowDown /></span>
-
+            <span className="input-group-text">
+              <HiMiniMagnifyingGlass />
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Students"
+            />
+            <span className="input-group-text">
+              <IoIosArrowDown />
+            </span>
           </div>
         </div>
         <div className="col-lg-6">
           <div className="input-group">
-            <span className="input-group-text"><HiMiniMagnifyingGlass /></span>
-            <input type="text" className="form-control" placeholder="Search Assignments" />
-            <span className="input-group-text"><IoIosArrowDown /></span>
+            <span className="input-group-text">
+              <HiMiniMagnifyingGlass />
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Assignments"
+            />
+            <span className="input-group-text">
+              <IoIosArrowDown />
+            </span>
           </div>
         </div>
       </div>
 
-      <button id="wd-add-assignment-btn" className="btn btn-lg btn-secondary me-1">
-          <CiFilter className="position-relative me-2" style={{ bottom: "1px" }} />
-          Apply Filters
-    </button>
+      <button
+        id="wd-add-assignment-btn"
+        className="btn btn-lg btn-secondary me-1"
+      >
+        <CiFilter
+          className="position-relative me-2"
+          style={{ bottom: "1px" }}
+        />
+        Apply Filters
+      </button>
 
       <div className="table-responsive">
         <table className="table table-striped">
           <thead>
             <tr>
               <th>Student Name</th>
-              <th>A1 SETUP</th>
-              <th>A2 HTML 2</th>
-              <th>A3 CSS 3</th>
-              <th>A4 BOOTSTRAP</th>
+              {courseAssignments.map((assignment) => (
+                <th key={assignment._id}>{assignment.title}</th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>John Doe</td>
-              <td><input type="text" className="form-control" defaultValue="90" /></td>
-              <td>85</td>
-              <td>92</td>
-              <td>88</td>
-
-            </tr>
-            <tr>
-              <td>Jane Smith</td>
-              <td>87</td>
-              <td>89</td>
-              <td>91</td>
-              <td>90</td>
-
-            </tr>
-
+            {enrolledStudents.map((student) => (
+              <tr key={student?._id ?? "unknown"}>
+                <td>
+                  {student?.firstName} {student?.lastName}
+                </td>
+                {courseAssignments.map((assignment) => {
+                  const gradeRecord = db.grades.find(
+                    (grade) =>
+                      grade.student === student?._id &&
+                      grade.assignment === assignment._id
+                  );
+                  return (
+                    <td key={assignment._id}>
+                      {gradeRecord ? gradeRecord.grade : "N/A"}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
